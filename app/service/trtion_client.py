316a88ed94fbfe1e7fc9b29f83efe2fd461feeeb
zzin <8639sung@gmail.com>
2024-11-11 02:09:31 +0900

    FIX: fix grpc port error

--- app/service/trtion_client.py
+++ app/service/trtion_client.py
@@ -1,30 +1,23 @@
 import tritonclient.grpc as grpcclient
-import numpy as np
-
-import app.config as config
+import config as config
 
 
-class OCRClient:
-    def __init__(self, server_url=config.INFERENCE_SERVER_URL):
+class TritonClient:
+    def __init__(self, server_url=config.INFERENCE_SERVER_GRPC_URL):
         self.client = grpcclient.InferenceServerClient(url=server_url)
 
-    def preprocess(self, image):
-        return image  
-
     def detect_text(self, image):
-        processed_image = self.preprocess(image)
-        inputs = [grpcclient.InferInput('input', processed_image.shape, "FP32")]
-        inputs[0].set_data_from_numpy(processed_image)
-        outputs = [grpcclient.InferRequestedOutput('output')]
+        inputs = [grpcclient.InferInput('x', image.shape, "FP32")]
+        inputs[0].set_data_from_numpy(image)
+        outputs = [grpcclient.InferRequestedOutput('sigmoid_0.tmp_0')]
         response = self.client.infer(model_name="det_model", inputs=inputs, outputs=outputs)
-        detection_result = response.as_numpy('output')
+        detection_result = response.as_numpy('sigmoid_0.tmp_0')
         return detection_result  
 
     def recognize_text(self, image):
-        processed_image = self.preprocess(image)
-        inputs = [grpcclient.InferInput('input', processed_image.shape, "FP32")]
-        inputs[0].set_data_from_numpy(processed_image)
-        outputs = [grpcclient.InferRequestedOutput('output')]
+        inputs = [grpcclient.InferInput('x', image.shape, "FP32")]
+        inputs[0].set_data_from_numpy(image)
+        outputs = [grpcclient.InferRequestedOutput('softmax_2.tmp_0')]
         response = self.client.infer(model_name="rec_model", inputs=inputs, outputs=outputs)
-        recognition_result = response.as_numpy('output')
+        recognition_result = response.as_numpy('softmax_2.tmp_0')
         return recognition_result 
