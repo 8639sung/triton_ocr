d94ef2f3fda8a7abc3f4a7abf30a95629f95048b
zzin <8639sung@gmail.com>
2024-11-10 20:07:17 +0900

    DOCS: add model config.pbtxt

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
-run_triton:
-	docker run -it --gpus all --shm-size=512m --rm -p8000:8000 -p8001:8001 -p8002:8002 -v ./model_repository:/models nvcr.io/nvidia/tritonserver:24.10-py3
-
-deploy_model:
-	tritonserver --model-repository=./model_repository
+deploy_triton:
+	docker run -d -it --gpus all -p 8000:8000 -p 8001:8001 -p 8002:8002 \
+  -v $(pwd)/model_repository:/models nvcr.io/nvidia/tritonserver:24.10-py3 \
+  tritonserver --model-repository=/models
--- model_convert/model_convert.py
+++ model_convert/model_convert.py
@@ -1,14 +1,42 @@
-import paddle
+import onnx
 import paddle2onnx
 
 paddle_detection_model_path = "detection_model/inference.pdmodel"
 paddle_detection_params_path = "detection_model/inference.pdiparams"
-onnx_detection_model_path = "model_repository/det_model/1/det_model.onnx"
+onnx_detection_model_path = "../model_repository/det_model/1/det_model.onnx"
 
 paddle2onnx.export(model_file=paddle_detection_model_path, save_file=onnx_detection_model_path, params_file=paddle_detection_params_path)
+model = onnx.load(onnx_detection_model_path)
+print("Inputs:")
+for inp in model.graph.input:
+    name = inp.name
+    dims = [dim.dim_value for dim in inp.type.tensor_type.shape.dim]
+    dtype = inp.type.tensor_type.elem_type
+    print(f"Name: {name}, Shape: {dims}, Type: {dtype}")
+
+print("\nOutputs:")
+for out in model.graph.output:
+    name = out.name
+    dims = [dim.dim_value for dim in out.type.tensor_type.shape.dim]
+    dtype = out.type.tensor_type.elem_type
+    print(f"Name: {name}, Shape: {dims}, Type: {dtype}")
 
 paddle_recognition_model_path = "recognition_model/inference.pdmodel"
 paddle_recognition_params_path = "recognition_model/inference.pdiparams"
-onnx_recognition_model_path = "model_repository/rec_model/1/rec_model.onnx"
+onnx_recognition_model_path = "../model_repository/rec_model/1/rec_model.onnx"
 
 paddle2onnx.export(model_file=paddle_recognition_model_path, save_file=onnx_recognition_model_path, params_file=paddle_recognition_params_path)
+model = onnx.load(onnx_recognition_model_path)
+print("Inputs:")
+for inp in model.graph.input:
+    name = inp.name
+    dims = [dim.dim_value for dim in inp.type.tensor_type.shape.dim]
+    dtype = inp.type.tensor_type.elem_type
+    print(f"Name: {name}, Shape: {dims}, Type: {dtype}")
+
+print("\nOutputs:")
+for out in model.graph.output:
+    name = out.name
+    dims = [dim.dim_value for dim in out.type.tensor_type.shape.dim]
+    dtype = out.type.tensor_type.elem_type
+    print(f"Name: {name}, Shape: {dims}, Type: {dtype}")
